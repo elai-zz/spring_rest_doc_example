4bf0a1ae646f4e2675b122e5d54a4cb663a1d280
elai <estella@Estellas-MacBook-Air.local>
2016-06-01 23:21:47 -0700

    working example of spring rest docs

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,6 +1,8 @@
 package hello;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
+    public ResponseEntity<String> greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
                         Model model) {
         if (name.equals("nobody")) {
-            throw new IllegalArgumentException("Application does not say hi to nobody");
+            //throw new IllegalArgumentException("Application does not say hi to nobody");
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
         } else {
             model.addAttribute("name", name);
-            return "greeting";
+            return new ResponseEntity<String>("greeting", HttpStatus.OK);
         }
 
     }
--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -1,18 +1,26 @@
 package hello;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.restdocs.RestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by estella on 5/30/16.
@@ -23,20 +31,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringApplicationConfiguration(classes = Application.class)
 public class HelloControllerTest {
 
+    @Rule
+    public final RestDocumentation restDocumentation = new RestDocumentation("build/generated-snippets");
+
     private MockMvc mockMVC;
+
+    private RestDocumentationResultHandler document;
 
     @Autowired
     private WebApplicationContext context;
 
     @Before
     public void setup() throws Exception {
-        this.mockMVC = MockMvcBuilders.standaloneSetup(context).build();
+        this.document = document("{method-name}",
+                preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()));
+        this.mockMVC = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .alwaysDo(this.document)
+                .build();
     }
 
     @Test
     public void TestNormalGreeting() throws Exception {
-        this.mockMVC.perform(get("/greeting"))
-                .andExpect(status().isOk());
-
+        this.mockMVC.perform(get("/greeting")).andExpect(status().isOk());
     }
+
+    @Test
+    public void TestNobodyGreeting() throws Exception {
+        this.mockMVC.perform(get("/greeting?name=nobody")).andExpect(status().is4xxClientError());
+    }
+
 }
